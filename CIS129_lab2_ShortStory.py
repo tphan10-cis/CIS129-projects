085700a7250ca500430e521cf6ea0c64c79b9f13
tphan10-cis <tphan10@mail.pima.edu>
2024-09-05 10:47:42 -0700

    Update CIS129_lab2_ShortStory.py
    The sentences are able to move down after a finish sentence by using \n.

--- CIS129_lab2_ShortStory.py
+++ CIS129_lab2_ShortStory.py
@@ -1,1 +1,3 @@
-print('Hello, World!')
+#Author: Thy Phan
+#CIS 129
+print("I find myself pondering about life. \nI was imagining how I could reach my dream. \nBut what's dream that I could think of that make my life a bit better? \nI pondering to myself.")
